8c3ba4546164d1366a1e9f70d892118169fbcbd9
ranebrown <rane.brown@gmail.com>
2016-01-30 18:07:22 -0700

    Added function outlines and basic definitions

---
+++ src/asf/avr32/drivers/motordriver/motor.c
@@ -0,0 +1,70 @@
+#include "motor.h"
+
+void initMotor(long baud, unsigned char receivePin, unsigned char transmitPin, unsigned char resetPin) {
+
+}
+
+char getFirmwareVersion() {
+
+}
+
+int getErrors() {
+
+}
+
+int getConfigurationParameter(int parameter) {
+
+}
+
+int	setConfigurationParameter(int parameter, int value) {
+
+}
+
+void setM0Speed(int speed) {
+
+}
+
+void setM1Speed(int speed) {
+
+}
+
+void setSpeeds(int m0Speed, int m1Speed) {
+
+}
+
+void setM0Brake(unsigned char brake) {
+
+}
+
+void setM1Brake(unsigned char brake) {
+
+}
+
+void setBrakes(unsigned char m0Brake, unsigned char m1Brake) {
+
+}
+
+unsigned char getM0Current() {
+
+}
+
+unsigned char getM1Current() {
+
+}
+
+unsigned int getM0CurrentMilliamps() {
+
+}
+
+unsigned int getM1CurrentMilliamps() {
+
+}
+
+unsigned char getM0Speed() {
+
+}
+
+unsigned char getM1Speed() {
+
+}
+
